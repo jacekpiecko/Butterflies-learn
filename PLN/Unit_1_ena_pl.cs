dc5e1998746404ec7cedd6359d18bad6f1fa3c3c
pepel <30470281+jacekpiecko@users.noreply.github.com>
2020-07-04 14:49:04 +0200

    Add files via upload

---
+++ PLN/Unit_1_ena_pl.cs
@@ -0,0 +1,513 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PLN
+{
+   public  class Unit_1_eng_pl
+    {
+        public string Slowa(int liczba)
+        {
+            switch (liczba)
+            {
+                case 0:
+                    return "aktorka";
+                case 1:
+                    return "koszyk";
+                case 2:
+                    return "plaża";
+                case 3:
+                    return "rower";
+                case 4:
+                    return "data urodzenia";
+                case 5:
+                    return "dzielony przez";
+                case 6:
+                    return "równać się";
+                case 7:
+                    return "ulubiony";
+                case 8:
+                    return "pierwszy";
+                case 9:
+                    return "połowa, pół";
+                case 10:
+                    return "święto niepodlęgłości";
+                case 11:
+                    return "kwadrans";
+                case 12:
+                    return "drugi";
+                case 13:
+                    return "deskorolka";
+                case 14:
+                    return "trzeci";
+                case 15:
+                    return "spragniony";
+                case 16:
+                    return "buty sportowe do biegania";
+                case 17:
+                    return "razy";
+                case 18:
+                    return "dwunasty";
+                case 19:
+                    return "dwudziesty";
+                case 20:
+                    return "pozwalać, zezwalać";
+                case 21:
+                    return "samotnie, w pojedynkę";
+                case 22:
+                    return "wygląd";
+                case 23:
+                    return "atrakcyjny";
+                case 24:
+                    return "ciotka";
+                case 25:
+                    return "broda";
+                case 26:
+                    return "dzwonek";
+                case 27:
+                    return "organizacja charytatywna";
+                case 28:
+                    return "wybierać";
+                case 29:
+                    return "Boże Narodzenie";
+                case 30:
+                    return "pochodzić z";
+                case 31:
+                    return "społeczność";
+                case 32:
+                    return "konkurs, rywalizacja";
+                case 33:
+                    return "para";
+                case 34:
+                    return "kuzyn";
+                case 35:
+                    return "córka";
+                case 36:
+                    return "decyzja";
+                case 37:
+                    return "róźny";
+                case 38:
+                    return "obiad";
+                case 39:
+                    return "danie";
+                case 40:
+                    return "sukienka";
+                case 41:
+                    return "księżna";
+                case 42:
+                    return "obowiązek";
+                case 43:
+                    return "najstarszy";
+                case 44:
+                    return "wybory";
+                case 45:
+                    return "lubić, znajdować (w czymś) przyjemność";
+                case 46:
+                    return "wydarzenie, impreza";
+                case 47:
+                    return "podniecający, ciekawy";
+                case 48:
+                    return "latać";
+                case 49:
+                    return "czas wolny";
+                case 50:
+                    return "wstawać";
+                case 51:
+                    return "rząd";
+                case 52:
+                    return "wnuczka";
+                case 53:
+                    return "babcia, dziadek";
+                case 54:
+                    return "pradziadek, prababcia";
+                case 55:
+                    return "dorastać";
+                case 56:
+                    return "nie cierpięć";
+                case 57:
+                    return "praca domowa";
+                case 58:
+                    return "mąż";
+                case 59:
+                    return "osoba przeprowadzajaca wywiad";
+                case 60:
+                    return "król";
+                case 61:
+                    return "prawo";
+                case 62:
+                    return "przywódca";
+                case 63:
+                    return "miejscowy, lokalny";
+                case 64:
+                    return "żonaty, zamężna";
+                case 65:
+                    return "mecz";
+                case 66:
+                    return "matematyka";
+                case 67:
+                    return "znaczyć, wyrażać";
+                case 68:
+                    return "członek";
+                case 69:
+                    return "wspominać (o kimś lub o czymś), nadmieniać";
+                case 70:
+                    return "tęsknić (za czymś)";
+                case 71:
+                    return "nowocześny";
+                case 72:
+                    return "wąsy";
+                case 73:
+                    return "sąsiad";
+                case 74:
+                    return "siostrzenieć, bratanek";
+                case 75:
+                    return "gazeta";
+                case 76:
+                    return "siostrzenica, bratanek";
+                case 77:
+                    return "hasłaśliwy, głośny";
+                case 78:
+                    return "sposobność, okazja";
+                case 79:
+                    return "urzędnik";
+                case 80:
+                    return "staromodny, staroświecki";
+                case 81:
+                    return "zwykty, normalny";
+                case 82:
+                    return "pierwotnie";
+                case 83:
+                    return "rodzic";
+                case 84:
+                    return "ludzie";
+                case 85:
+                    return "okres";
+                case 86:
+                    return "zwierzątko domowe";
+                case 87:
+                    return "ziemniak";
+                case 88:
+                    return "władza";
+                case 89:
+                    return "ćwiczyć, trenować";
+                case 90:
+                    return "prepare";
+                case 91:
+                    return "książę";
+                case 92:
+                    return "królowa";
+                case 93:
+                    return "cichy, spokojny";
+                case 94:
+                    return "powód, przyczyna";
+                case 95:
+                    return "królewski";
+                case 96:
+                    return "kanapka";
+                case 97:
+                    return "straszny, przerażający";
+                case 98:
+                    return "prosty, nieskomplikowany";
+                case 99:
+                    return "jazda na nartach";
+                case 100:
+                    return "syn";
+                case 101:
+                    return "przyrodni brat";
+                case 102:
+                    return "ojczym ";
+                case 103:
+                    return "macocha ";
+                case 104:
+                    return "garnitur";
+                case 105:
+                    return "popierać";
+                case 106:
+                    return "turystyka";
+                case 107:
+                    return "bliźniak";
+                case 108:
+                    return "wujek";
+                case 109:
+                    return "niezwykły, niecodzienny";
+                case 110:
+                    return "zwykle";
+                case 111:
+                    return "siatkówka";
+                case 112:
+                    return "głosować";
+                case 113:
+                    return "co";
+                case 114:
+                    return "kiedy";
+                case 115:
+                    return "gdzie";
+                case 116:
+                    return "który";
+                case 117:
+                    return "kto";
+                case 118:
+                    return "czyj";
+                case 119:
+                    return "żona";
+                case 120:
+                    return "słowa, wyraz";
+                case 121:
+                    return "ubierać się";
+                case 122:
+                    return "przstojny";
+                case 123:
+                    return "wstępny, wprowadzający";
+                case 124:
+                    return "zwykły";
+                case 125:
+                    return "dorosły";
+                case 126:
+                    return "niemowlę";
+                case 127:
+                    return "brat";
+                case 128:
+                    return "dzieci";
+                case 129:
+                    return "para";
+                case 130:
+                    return "tata";
+                case 131:
+                    return "pies";
+                case 132:
+                    return "rodzina";
+                case 133:
+                    return "ojciec";
+                case 134:
+                    return "kolega, przyjaciel";
+                case 135:
+                    return "dziadek";
+                case 136:
+                    return "babcia";
+                case 137:
+                    return "wnuk";
+                case 138:
+                    return "pradziadek";
+                case 139:
+                    return "prababcia";
+                case 140:
+                    return "mąż";
+                case 141:
+                    return "kochać, miłość";
+                case 142:
+                    return "żonaty, zamężna";
+                case 143:
+                    return "członek";
+                case 144:
+                    return "mama";
+                case 145:
+                    return "osoba";
+                case 146:
+                    return "zwierzątko domowe";
+                case 147:
+                    return "siostra";
+                case 148:
+                    return "bliźniak";
+                case 149:
+                    return "gość";
+                case 150:
+                    return "śniadanie";
+                case 151:
+                    return "czekolada";
+                case 152:
+                    return "gotować";
+                case 153:
+                    return "jedzenia";
+                case 154:
+                    return "lancz";
+                case 155:
+                    return "pomidor";
+                case 156:
+                    return "piękny";
+                case 157:
+                    return "nudny";
+                case 158:
+                    return "wczesny";
+                case 159:
+                    return "łatwy";
+                case 160:
+                    return "słynny, znany";
+                case 161:
+                    return "wolny, darmowy";
+                case 162:
+                    return "inteligentny";
+                case 163:
+                    return "ciekawy, interesujący";
+                case 164:
+                    return "spóźniony";
+                case 165:
+                    return "nowoczesny";
+                case 166:
+                    return "naturalny";
+                case 167:
+                    return "nowy";
+                case 168:
+                    return "przyjemny, miły";
+                case 169:
+                    return "normalny";
+                case 170:
+                    return "staromodny, staroświecki";
+                case 171:
+                    return "taki sam";
+                case 172:
+                    return "krótki, niski";
+                case 173:
+                    return "mały";
+                case 174:
+                    return "prawdziwy";
+                case 175:
+                    return "robić";
+                case 176:
+                    return "konczyć";
+                case 177:
+                    return "wychodzić (np. na miasto)";
+                case 178:
+                    return "wyjeżdżać, odchodzić";
+                case 179:
+                    return "lubić";
+                case 180:
+                    return "słuchać";
+                case 181:
+                    return "poznawać, spotykać";
+                case 182:
+                    return "odpoczywać";
+                case 183:
+                    return "mówić";
+                case 184:
+                    return "rozmawiać, mówić (z kimś)";
+                case 185:
+                    return "uczyć się, studiować";
+                case 186:
+                    return "uczyć (kogoś)";
+                case 187:
+                    return "podróżować";
+                case 188:
+                    return "używać";
+                case 189:
+                    return "odwiedzać";
+                case 190:
+                    return "chodzić, iść";
+                case 191:
+                    return "oglądać";
+                case 192:
+                    return "pracować";
+                case 193:
+                    return "pisać";
+                case 194:
+                    return "ja";
+                case 195:
+                    return "ty";
+                case 196:
+                    return "ona";
+                case 197:
+                    return "on";
+                case 198:
+                    return "ono";
+                case 199:
+                    return "ja jestem";
+                case 200:
+                    return "ty jesteś";
+                case 201:
+                    return "ona jest";
+                case 202:
+                    return "on jest";
+                case 203:
+                    return "to jest";
+                case 204:
+                    return "oni, one";
+                case 205:
+                    return "oni są, one są";
+                case 206:
+                    return "mój";
+                case 207:
+                    return "twoje, wasz";
+                case 208:
+                    return "jego";
+                case 209:
+                    return "jej";
+                case 210:
+                    return "jego";
+                case 211:
+                    return "nasz";
+                case 212:
+                    return "ich";
+                case 213:
+                    return "iść do";
+                case 214:
+                    return "telewizja";
+                case 215:
+                    return "imię";
+                case 216:
+                    return "klasa";
+                case 217:
+                    return "naprawdę";
+                case 218:
+                    return "wiedzieć";
+                case 219:
+                    return "z, do, od, ze, przez, ode";
+                case 220:
+                    return "stary";
+                case 221:
+                    return "słuchać";
+                case 222:
+                    return "piosenkarz";
+                case 223:
+                    return "szkoła";
+                case 224:
+                    return "muzyka";
+            }
+            return null;
+        }
+        public string Slowa_dodakowie(int liczba)
+        {
+            switch (liczba)
+            {
+                case 0:
+                    return "przyrodni sąsiad";
+                case 1:
+                    return "przyrodni syn";
+            }
+            return null;
+        }
+        public string Zdania(int liczba)
+        {
+            switch (liczba)
+            {
+                case 0:
+                    return "ja idem do szkoły";
+                case 1:
+                    return "ogładam telewizję";
+                case 2:
+                    return "ja ucze się gotować";
+                case 3:
+                    return "ja nie pracuje";
+                case 4:
+                    return "ona idzie do szkoły ";
+                case 5:
+                    return "on ogląda telewizję";
+                case 6:
+                    return "ona uczy się gotować";
+                case 7:
+                    return "on nie pracuje";
+                case 8:
+                    return "mam na imię";
+                case 9:
+                    return "Miłó cię poznać";
+                case 10:
+                    return "w której jesteś w klasie?";
+                case 11:
+                    return "Czy znasz  ją";
+                case 12:
+                    return "Skąd jesteś?";
+                case 13:
+                    return "Słucham muzyki";
+            }
+            return null;
+        }
+    }
+}
