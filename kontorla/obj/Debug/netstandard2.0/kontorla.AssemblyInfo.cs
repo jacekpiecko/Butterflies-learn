a91265c9195c76b7e35ba6aa542093b7f8f04fc0
pepel <30470281+jacekpiecko@users.noreply.github.com>
2020-07-04 15:33:52 +0200

    Add files via upload

--- kontorla/obj/Debug/netstandard2.0/kontorla.AssemblyInfo.cs
+++ kontorla/obj/Debug/netstandard2.0/kontorla.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("kontorla")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Kontorla")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("kontorla")]
-[assembly: System.Reflection.AssemblyTitleAttribute("kontorla")]
+[assembly: System.Reflection.AssemblyProductAttribute("Kontorla")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Kontorla")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Wygenerowane przez klasę WriteCodeFragment programu MSBuild.
