3d6e411c0f0155177c8511eb801a9f62864e17bc
jacek piecko <jacekpiecko@wp.pl>
2020-07-20 13:08:58 +0200

    now

--- Eng/Unit_1_eng.cs
+++ Eng/Unit_1_eng.cs
@@ -2,518 +2,109 @@
 {
 	public class Unit_1_eng
 	{
-		public int liczba_slow = 224;
-		public int liczba_dodakowych_slow = 1;
-		public int liczba_zdan = 13;
+		byte _a;
+		readonly Random random = new Random();
+		public byte A { get { return _a; } private set { _a = value; } }
+		/// <summary>
+		/// ustawia radomowie słowo
+		/// </summary>
+		/// <returns></returns>
+		public string Slowna()
+		{
+			//radomowa liczba do 50
+			A = (byte)random.Next(50);
+			//radowa liczba
+			return Slowa_(A);
+		}
 
-		public string Slowa(int liczba)
+		/// <summary>
+		/// głownia metoda slowna
+		/// </summary>
+		/// <param name="liczba"></param>
+		/// <returns></returns>
+		public string Slowna(byte liczba)
 		{
-			switch (liczba)
-			{
-				case 0:
-					return "actress";
-				case 1:
-					return "basket";
-				case 2:
-					return "beach";
-				case 3:
-					return "bike";
-				case 4:
-					return "date of birth";
-				case 5:
-					return "divided by";
-				case 6:
-					return "equal";
-				case 7:
-					return "favourite";
-				case 8:
-					return "first";
-				case 9:
-					return "half";
-				case 10:
-					return "independence day";
-				case 11:
-					return "quarter";
-				case 12:
-					return "second";
-				case 13:
-					return "skateboard";
-				case 14:
-					return "third";
-				case 15:
-					return "thirsty";
-				case 16:
-					return "trainers";
-				case 17:
-					return "times";
-				case 18:
-					return "twelfth";
-				case 19:
-					return "twentieth";
-				case 20:
-					return "allow";
-				case 21:
-					return "alone";
-				case 22:
-					return "appearance";
-				case 23:
-					return "attractive";
-				case 24:
-					return "aunt";
-				case 25:
-					return "beard";
-				case 26:
-					return "bell";
-				case 27:
-					return "charity";
-				case 28:
-					return "choose";
-				case 29:
-					return "christmas";
-				case 30:
-					return "come from";
-				case 31:
-					return "community";
-				case 32:
-					return "competition";
-				case 33:
-					return "couple";
-				case 34:
-					return "cousin";
-				case 35:
-					return "daughter";
-				case 36:
-					return "decision";
-				case 37:
-					return "different";
-				case 38:
-					return "dinner";
-				case 39:
-					return "dish";
-				case 40:
-					return "dress";
-				case 41:
-					return "duchess";
-				case 42:
-					return "duty";
-				case 43:
-					return "eldest";
-				case 44:
-					return "election";
-				case 45:
-					return "enjoy";
-				case 46:
-					return "event";
-				case 47:
-					return "exciting";
-				case 48:
-					return "fly";
-				case 49:
-					return "free time";
-				case 50:
-					return "get up";
-				case 51:
-					return "government";
-				case 52:
-					return "granddaughter";
-				case 53:
-					return "grandparent";
-				case 54:
-					return "great-grandparent";
-				case 55:
-					return "grow up";
-				case 56:
-					return "hate";
-				case 57:
-					return "homework";
-				case 58:
-					return "husband";
-				case 59:
-					return "interviewer";
-				case 60:
-					return "king";
-				case 61:
-					return "law";
-				case 62:
-					return "leader";
-				case 63:
-					return "local";
-				case 64:
-					return "married";
-				case 65:
-					return "match";
-				case 66:
-					return "maths";
-				case 67:
-					return "mean";
-				case 68:
-					return "member";
-				case 69:
-					return "mentions";
-				case 70:
-					return "miss";
-				case 71:
-					return "modern";
-				case 72:
-					return "moustache";
-				case 73:
-					return "neighbour";
-				case 74:
-					return "nephew";
-				case 75:
-					return "newspaper";
-				case 76:
-					return "niece";
-				case 77:
-					return "noisy";
-				case 78:
-					return "occasion";
-				case 79:
-					return "official";
-				case 80:
-					return "old-fashioned";
-				case 81:
-					return "ordinary";
-				case 82:
-					return "originally";
-				case 83:
-					return "parent";
-				case 84:
-					return "people";
-				case 85:
-					return "period";
-				case 86:
-					return "pet";
-				case 87:
-					return "potato";
-				case 88:
-					return "power";
-				case 89:
-					return "practise";
-				case 90:
-					return "prepare";
-				case 91:
-					return "prince";
-				case 92:
-					return "queen";
-				case 93:
-					return "guiet";
-				case 94:
-					return "reason";
-				case 95:
-					return "royal";
-				case 96:
-					return "sandwich";
-				case 97:
-					return "scary";
-				case 98:
-					return "simple";
-				case 99:
-					return "skiing";
-				case 100:
-					return "son";
-				case 101:
-					return "stepbrother";
-				case 102:
-					return "stepdad";
-				case 103:
-					return "stepmother";
-				case 104:
-					return "suit";
-				case 105:
-					return "support";//
-				case 106:
-					return "tourism";
-				case 107:
-					return "twin";
-				case 108:
-					return "uncle";
-				case 109:
-					return "unusual";
-				case 110:
-					return "usually";
-				case 111:
-					return "volleyball";
-				case 112:
-					return "vote";
-				case 113:
-					return "what";
-				case 114:
-					return "when";
-				case 115:
-					return "where";
-				case 116:
-					return "which";
-				case 117:
-					return "who";
-				case 118:
-					return "whose";
-				case 119:
-					return "wife";
-				case 120:
-					return "word";
-				case 121:
-					return "get dressed";
-				case 122:
-					return "handsome";
-				case 123:
-					return "introductory";
-				case 124:
-					return "usal";
-				case 125:
-					return "adult";
-				case 126:
-					return "baby";
-				case 127:
-					return "brother";
-				case 128:
-					return "children";
-				case 129:
-					return "couple";//
-				case 130:
-					return "dad";//
-				case 131:
-					return "dog";
-				case 132:
-					return "family";
-				case 133:
-					return "father";
-				case 134:
-					return "friend";
-				case 135:
-					return "grandfather";
-				case 136:
-					return "grandmother";
-				case 137:
-					return "grandson";//
-				case 138:
-					return "great-grandfather";
-				case 139:
-					return "great-grandmother";
-				case 140:
-					return "husband";
-				case 141:
-					return "love";
-				case 142:
-					return "married";
-				case 143:
-					return "member";
-				case 144:
-					return "mum";
-				case 145:
-					return "person";
-				case 146:
-					return "pet";
-				case 147:
-					return "sister";
-				case 148:
-					return "twin";
-				case 149:
-					return "visitor";
-				case 150:
-					return "breakfast";
-				case 151:
-					return "chocolate";
-				case 152:
-					return "cook";
-				case 153:
-					return "food";
-				case 154:
-					return "luncheon";
-				case 155:
-					return "tomato";
-				case 156:
-					return "beautiful";
-				case 157:
-					return "boring";
-				case 158:
-					return "early";
-				case 159:
-					return "easy";
-				case 160:
-					return "famous";
-				case 161:
-					return "free";
-				case 162:
-					return "intelligent";
-				case 163:
-					return "interesting";
-				case 164:
-					return "late";
-				case 165:
-					return "modern";
-				case 166:
-					return "natural";
-				case 167:
-					return "new";
-				case 168:
-					return "nice";
-				case 169:
-					return "normal";
-				case 170:
-					return "ordinary";
-				case 171:
-					return "same";
-				case 172:
-					return "short";
-				case 173:
-					return "small";
-				case 174:
-					return "true";
-				case 175:
-					return "do";
-				case 176:
-					return "finish";
-				case 177:
-					return "go out";
-				case 178:
-					return "leave";
-				case 179:
-					return "like";
-				case 180:
-					return "listen";
-				case 181:
-					return "meet";
-				case 182:
-					return "relax";
-				case 183:
-					return "say";
-				case 184:
-					return "speak";
-				case 185:
-					return "study";
-				case 186:
-					return "teach";
-				case 187:
-					return "travel";
-				case 188:
-					return "use";
-				case 189:
-					return "visit";
-				case 190:
-					return "walk";
-				case 191:
-					return "watch";
-				case 192:
-					return "work";
-				case 193:
-					return "write";
-				case 194:
-					return "I";
-				case 195:
-					return "you";
-				case 196:
-					return "she";
-				case 197:
-					return "he";
-				case 198:
-					return "it";
-				case 199:
-					return "I am";
-				case 200:
-					return "you are";
-				case 201:
-					return "she is";
-				case 202:
-					return "he is";
-				case 203:
-					return "it is";
-				case 204:
-					return "they";
-				case 205:
-					return "they are";
-				case 206:
-					return "my";
-				case 207:
-					return "your";
-				case 208:
-					return "his";
-				case 209:
-					return "her";
-				case 210:
-					return "its";
-				case 211:
-					return "our";
-				case 212:
-					return "their";
-				case 213:
-					return "go to";
-				case 214:
-					return "TV";
-				case 215:
-					return "name";
-				case 216:
-					return "class";
-				case 217:
-					return "really";
-				case 218:
-					return "know";
-				case 219:
-					return "from";
-				case 220:
-					return "old";
-				case 221:
-					return "listen";
-				case 222:
-					return "singers";
-				case 223:
-					return "school";
-				case 224:
-					return "music";
-				default:
-					break;
-			}
-			return null;
+			// ustawia liczbe do pamiecy
+			A = liczba;
+
+			//sprawdza czy liczba jest do 50 
+			return Slowa_(A);
+
 		}
-		public string Slowa_dodakowie(int liczba)
+		/// <summary>
+		/// wysyła Slowo do metody sprawdz. Nie używacz tego
+		/// </summary>
+		/// <param name="liczba"></param>
+		/// <returns></returns>
+		public string Slowa_sprawcz(byte liczba)
 		{
-			switch (liczba)
-			{
-				case 0:
-					return "stepneighbour";
-				case 1:
-					return "stepson";
-				default:
-					break;
-			}
-			return null;
+			//sprawdza czy liczba jest do 50 
+			return Slowa_(liczba);
 		}
-		public string Zdania(int liczba)
+
+		/// <summary>
+		/// ustawa liczby
+		/// </summary>
+		/// <param name="slowa"></param>
+		/// <returns></returns>
+		private string Slowa_(byte slowa)
 		{
-			switch (liczba)
+			return slowa switch
 			{
-				case 0:
-					return "I go to school";
-				case 1:
-					return "I watch TV";
-				case 2:
-					return "I study cook";
-				case 3:
-					return "I don't work";
-				case 4:
-					return "she goes to school";
-				case 5:
-					return "he watches TV";
-				case 6:
-					return "she studies cook";
-				case 7:
-					return "he doesn't work";
-				case 8:
-					return "My name";
-				case 9:
-					return "Nice to meet ouy";
-				case 10:
-					return "what class are you in?";
-				case 11:
-					return "Do you know his";
-				case 12:
-					return "where are you from?";
-				case 13:
-					return "listen of music";
-				default:
-					break;
-			}
-			return null;
+
+				1 => "and",
+				2 => "to",
+				3 => "in",
+				4 => "is",
+				5 => "you",
+				6 => "that",
+				7 => "it",
+				8 => "he",
+				9 => "was",
+				10 => "for",
+				11 => "on",
+				12 => "are",
+				13 => "as",
+				14 => "with",
+				15 => "his",
+				16 => "they",
+				17 => "I",
+				18 => "at",
+				19 => "be",
+				20 => "this",
+				21 => "had",
+				22 => "have",
+				23 => "from",
+				24 => "or",
+				25 => "one",
+				26 => "by",
+				27 => "word",
+				28 => "but",
+				29 => "not",
+				30 => "what",
+				31 => "all",
+				32 => "were",
+				33 => "we",
+				34 => "when",
+				35 => "your",
+				36 => "can",
+				37 => "said",
+				38 => "there",
+				39 => "use",
+				40 => "each",
+				41 => "which",
+				42 => "she",
+				43 => "do",
+				44 => "how",
+				45 => "their",
+				46 => "if",
+				47 => "will",
+				48 => "up",
+				49 => "other",
+				50 => "about",
+				_ => "" // wysyła błąd
+			};
 		}
+
 	}
 }
